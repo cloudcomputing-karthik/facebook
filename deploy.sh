a94eb0e94cfbc140ccc15eb1ffdfb7a21acd4d09
purandeswarreddy <gpurandeswarreddy95@gmail.com>
2023-06-14 17:55:08 +0530

    reverted revert "updated"
    This reverts commit 12fa0557a4d1733816fc66441a1670413fb0937d.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,1 @@
-some changes are done and development is going o
-
-This is the edited file alogn with new createion test.sh and test. java file
+some changes are done and development is going om
--- test.sh
+++
@@ -1,4 +0,0 @@
-hello this is the test file file
-test file
-test file
-
